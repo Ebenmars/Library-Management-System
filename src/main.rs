e937e2c4549a870e503c4a190014d8da8a9a2390
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2023-11-06 13:13:23 -0500

    Added More Titles and Displayed them

--- src/main.rs
+++ src/main.rs
@@ -33,33 +33,40 @@ fn main() {
    let mut book1  = Book::new("Animal Farm".to_string(),"George Orswell".to_string(),1945);
    let mut book2  = Book::new("Naruto".to_string(),"Masashi Kishimoto".to_string(),1999);
    let mut book3  = Book::new("Vagabond".to_string(),"Takehiko Inoue".to_string(),1998);
-   let mut book4  = Book::new("Animal Farm".to_string(),"George Orswell".to_string(),1945);
-   let mut book5  = Book::new("Animal Farm".to_string(),"George Orswell".to_string(),1945);
+
    
 
    println!();
    println!("Title: {:?}", book1.title);
    println!("Author: {:?}", book1.author);
    println!("Year Published: {:?}", book1.year_published);
+
    let is_out = book1.is_checked_out;
-   if is_out{
-    println!("Is it checked out: Yes");
-    println!()
-   }
-   else{
-    println!("Is it checked out: No");
-    println!()
-   }
+   println!();
+
+
+   println!("Title: {:?}", book2.title);
+   println!("Author: {:?}", book2.author);
+   println!("Year Published: {:?}", book2.year_published);
+   println!();
+
+   println!("Title: {:?}", book3.title);
+   println!("Author: {:?}", book3.author);
+   println!("Year Published: {:?}", book3.year_published);
+   println!();
+
 
    book1.check_out();
 
    println!("Do you guys have Animal Farm?");
 
    if is_out {
-    println!("Is it checked out?\n\nThe book is not checked out")
+    println!("Is it checked out?\n\nThe book is not checked out");
+    println!();
    }
    else{
-    println!("Is it checked out?\n\nThe book is checked out")
+    println!("Is it checked out?\n\nThe book is checked out");
+    println!();
    }
   
    
